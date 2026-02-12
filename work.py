b0a3d2351da82c939cefcd9413b3f3a08ea8929f
blueeejay <jadec153@nycstudents.net>
2026-02-12 10:04:22 -0500

    Update work.py

---
+++ work.py
@@ -0,0 +1,12 @@
+""" You will be creating a little store in your terminal. You will have a list of
+dictionaries that will be displayed to the user. Each Dictionary will have at
+least 3 properties (name, price and whatever you want)
+PART ONE:
+The user will select one item to purchase. You will then show the user
+ONLY the name of the item they purchased. You will need to use the item
+index to accomplish this task. """
+item = {
+    "weapon name" = "laer blade",
+    "price" = 33.99,
+    "weapon type" = "sword"
+}
